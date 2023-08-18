d841d44b5e630e3e2a8e86ac5f23f9f6906f0fcf
Rick Huang <rick.liyue.huang@gmail.com>
2023-08-18 15:02:08 +1000

    feat: create FigureList component

--- src/app/(route)/(routes)/page.tsx
+++ src/app/(route)/(routes)/page.tsx
@@ -1,13 +1,44 @@
 import SearchInput from '@/components/SearchInput';
 import prismaDB from '@/lib/prisma-db';
 import Categories from '@/components/Categories';
+import FigureList from '@/components/FigureList';
 
-export default async function Home() {
+interface Props {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+export default async function Home({
+  searchParams: { categoryId, name },
+}: Props) {
   const categories = await prismaDB.category.findMany();
+
+  const data = await prismaDB.figure.findMany({
+    where: {
+      categoryId: categoryId,
+      name: {
+        search: name,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
   return (
     <div className="h-full p-4 space-y-4">
       <SearchInput />
       <Categories data={categories} />
+      <FigureList data={data} />
     </div>
   );
 }
